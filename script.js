6f4a213eb07f5bea227f98b2272438c4d13b5794
Vikalp Mishra <mishravikalp896@gmail.com>
2023-07-02 15:38:45 +0530

    script file complete

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
+// const notesContainer = document.querySelector(".notes-container");
+// const createBtn = document.querySelector('.btn');
+// let notes = document.querySelectorAll(".input-box");
+
+// function showNotes() {
+//     notesContainer.innerHTML = localStorage.getItem("notes")
+// }
+// showNotes();
+
+// function updateStorage() {
+//     localStorage.setItem("notes", notesContainer.innerHTML);
+// }
+
+
+// createBtn.addEventListener("click", () => {
+//     let inputBox = document.createElement("p");
+//     let img = document.createElement("img");
+//     inputBox.className = 'input-box';
+//     inputBox.setAttribute("contenteditable", "true");
+//     img.src = "delete.png";
+//     notesContainer.appendChild(inputBox).appendChild(img);
+// })
+
+// notesContainer.addEventListener('click', function (e) {
+//     if (e.target.tagName === 'IMG') {
+//         e.target.parentElement.remove()
+//         updateStorage()
+//     }
+//     else if (e.target.tagName === "p") {
+//         notes = document.querySelectorAll('.input-box')
+//         notes.forEach(nt => {
+//             nt.onkeyup = function () {
+//                 updateStorage();
+//             }
+//         })
+//     }
+// })
+
+// document.addEventListener("keydown", event => {
+//     if (event.key === "Enter") {
+//         document.execCommand("insertLineBreak");
+//         event.preventDefault();
+//     }
+// })
+
+
+
 const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector('.btn');
-let notes = document.querySelectorAll(".input-box");
+
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
 
 createBtn.addEventListener("click", () => {
     let inputBox = document.createElement("p");
@@ -9,10 +64,29 @@ createBtn.addEventListener("click", () => {
     inputBox.setAttribute("contenteditable", "true");
     img.src = "delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
-})
+
+    // Update the 'notes' collection after creating a new input-box
+    notes = document.querySelectorAll('.input-box');
+});
 
 notesContainer.addEventListener('click', function (e) {
     if (e.target.tagName === 'IMG') {
-        e.target.parentElement.remove()
+        e.target.parentElement.remove();
+        updateStorage();
     }
-})
+    else if (e.target.tagName === "P") {
+        notes = document.querySelectorAll('.input-box');
+        notes.forEach(nt => {
+            nt.onkeyup = function () {
+                updateStorage();
+            }
+        });
+    }
+});
+
+document.addEventListener("keydown", event => {
+    if (event.key === "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+});
